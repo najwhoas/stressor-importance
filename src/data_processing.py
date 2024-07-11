f039032a979a8cec717bd7afa1e75586cdb3605c
Naju <124247939+najwhoas@users.noreply.github.com>
2024-07-11 20:25:48 +0700

    Update data_processing.py

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,19 +1,18 @@
-from libs import *
 import pandas as pd
-def load_data(url):
-    data = pd.read_csv(url)
+
+def load_data(file_path):
+    data = pd.read_csv(file_path)
     return data
 
-def handle_outliers(data):
+def clean_data(data):
+    data['blood_pressure'] = data['blood_pressure'].map({1:0, 2:1, 3:2})
     for var in data.columns:
         Q1 = data[var].quantile(0.25)
         Q3 = data[var].quantile(0.75)
         IQR = Q3 - Q1
         maximum = Q3 + (1.5 * IQR)
         minimum = Q1 - (1.5 * IQR)
-        kondisi_lower_than = data[var] < minimum
-        kondisi_more_than = data[var] > maximum
-        data[var] = data[var].mask(cond=kondisi_more_than, other=maximum)
-        data[var] = data[var].mask(cond=kondisi_lower_than, other=minimum)
+        data[var] = data[var].mask(data[var] > maximum, maximum)
+        data[var] = data[var].mask(data[var] < minimum, minimum)
     data = data.astype('int64')
     return data
