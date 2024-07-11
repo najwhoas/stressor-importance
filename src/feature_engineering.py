ba7391cc1551710200f946469be6147ea2788f3f
Naju <124247939+najwhoas@users.noreply.github.com>
2024-07-11 20:26:39 +0700

    Update feature_engineering.py

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,8 +1,4 @@
-from libs import *
-def map_categorical(data, column, mapping):
-    data[column] = data[column].map(mapping)
-    return data
-
-def split_data(X, y, test_size=0.3, random_state=42):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
-    return X_train, X_test, y_train, y_test
+def extract_features(data):
+    X = data.drop('stress_level', axis=1)
+    y = data['stress_level']
+    return X, y
